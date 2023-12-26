d3a77cb6bbd7b99886b54fddf42d7d7047c92519
gülüzar doğan <dogangul778@gmail.com>
2023-12-26 23:09:43 +0300

    Revert "-"
    This reverts commit faaaeafd6d8840f3c507f27eedf9c3e47f7deadd.

--- kuafor/html/js/galeri.js
+++ kuafor/html/js/galeri.js
@@ -13,9 +13,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-function kapatBildirimCubugu() {
-    document.getElementById("bildirimCubugu").style.display = "none";
-}
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("bildirimCubugu").style.display = "block";
-});
