bfe01e1d831419eac9f41adc6500d5ebe45697e3
gülüzar doğan <dogangul778@gmail.com>
2024-01-12 21:47:31 +0300

    -

--- kuafor/html/js/uyeOl.js
+++ kuafor/html/js/uyeOl.js
@@ -1,19 +1,27 @@
-function handleSubmit() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const successMessage = document.getElementById('successMessage');
+function handleSubmit(event) {
+    event.preventDefault();
 
-    if (!firstName || !lastName || !email || !password || !phoneNumber) {
-        alert('Lütfen bütün alanları doldurun.');
-        return;
-    }
-    if (!/\d/.test(password)) {
-        alert('Şifrenizde en az bir rakam kullanmalısınız.');
-        return;
-    }
-    successMessage.textContent = 'Başarıyla üye oldunuz!';
+    const firstName = event.target.elements.firstName.value;
+    const lastName = event.target.elements.lastName.value;
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+    const userType = event.target.elements.userType.value;
+
+
+
+    showSuccessMessage(firstName);
+}
+
+function showSuccessMessage(userName) {
+    const successMessage = document.createElement('div');
+    successMessage.className = 'alert alert-success';
+    successMessage.innerHTML = `<strong>Üyelik Başarılı!</strong> Hoş geldin, ${userName}!`;
+
+    const appContainer = document.querySelector('.app-container');
+    appContainer.appendChild(successMessage);
+
+    setTimeout(() => {
+        successMessage.remove();
+    }, 5000);
 }
 
